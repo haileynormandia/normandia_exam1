a35ef96ddcf2fe91472321f0be65cb184ccb42d5
haileynormandia <hailey.normandia@bobcats.gcsu.edu>
2024-09-14 17:22:04 -0400

    hailey exam

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,12 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/exam')
+def exam():
+    return render_template('exam.html')
+
+@app.route('/index ')
+def index():
+    return render_template('index.html')
+
+
